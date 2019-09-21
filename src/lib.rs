ff3e0e4dad807c59e398a6b5288d1a144cb18969
Shubham Thakur <sbmthakur@gmail.com>
2019-09-21 19:04:53 +0530

    Use Iterator fns

--- src/lib.rs
+++ src/lib.rs
@@ -10,12 +10,18 @@ pub struct Config {
 
 
 impl Config {
-    pub fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough args");
-        }
-        let query = args[1].clone();
-        let filename = args[2].clone();
+    pub fn new(mut args: std::env::Args) -> Result<Config, &'static str> {
+        args.next();
+
+        let query = match args.next() {
+            Some(arg) => arg, 
+            None => return Err("Not a query string"),
+        };
+
+        let filename = match args.next() {
+            Some(arg) => arg, 
+            None => return Err("Not a filename"),
+        };
 
         let case_sensitive = env::var("CI").is_err();
 
@@ -40,13 +46,9 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut res = Vec::new();
-    for line in contents.lines() {
-        if line.contains(query) {
-            res.push(line);
-        }
-    }
-    res
+    contents.lines()
+        .filter(|line| line.contains(query))
+        .collect()
 }
 
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
